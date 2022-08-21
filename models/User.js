e8961754097ca3ad33f9f9f12f0729444db28a78
oak45985 <oak45985@gmail.com>
2022-08-21 15:23:33 -0500

    created routes, used sequelize, hid pws

---
+++ models/User.js
@@ -0,0 +1,62 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+//create our User model
+class User extends Model {}
+
+//define table columns and config
+User.init(
+    {
+        //define an id column
+        id: {
+            //use the special sequelize datatypes object provide what type of data it is
+            type: DataTypes.INTEGER,
+            //this is the equivalent of sql's NOT NULL option
+            allowNull: false,
+            //instruct that this is primary key
+            primaryKey: true,
+            //turn on auto increment
+            autoIncrement: true
+        },
+        //define a username column
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        //define an email column
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            //cannot be any duplicate email values in this table
+            unique: true,
+            //if allowNull is set to false, we can run our data thru validators before creating table data
+            validate: {
+                isEmail: true
+            }
+        },
+        //define a pw column
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                //this means the pw is at least four characters long
+                len: [4]
+            }
+        }
+    },
+    {
+        //Table config options go here
+        //pass in our imported sequelize connection (direct connection to our database)
+        sequelize,
+        // don't automatically create createdAt/updatedAt timestamp fields
+        timestamps: false,
+        //don't pluralize name of database table
+        freezeTableName: true,
+        //use underscores instead of camel-casing(ie 'comment_text not "commentText"
+        underscored: true,
+        //make it so our model name stays lowercase in the db
+        modelName: 'user'
+    }
+);
+
+module.exports = User;
