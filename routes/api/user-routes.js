121ba2da863a6c9be3addc71bb82c4c274ad858c
oak45985 <oak45985@gmail.com>
2022-08-21 20:39:07 -0500

    password items finished

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -50,6 +50,29 @@ router.post('/', (req, res) => {
     });
 });
 
+router.post('/login', (req, res) => {
+    // expects {email: 'email@email.com, password: 'pw'}
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address' });
+            return;
+        }
+        //verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password' });
+            return;
+        }
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+        //add comment syntax in front of this line the .then()
+        //res.json ({ user: dbUserData })
+    });
+});
+
 //PUT /api/users/1
 router.put('/:id', (req, res) => {
     //expects {username: 'name', email: 'email@email.com', password: 'pw'}
