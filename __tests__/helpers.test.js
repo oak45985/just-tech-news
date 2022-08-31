ba68a202cc37b4354d499bb2ffec7ad493b587c9
oak45985 <oak45985@gmail.com>
2022-08-31 15:02:08 -0500

    updated tests

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,4 +1,4 @@
-const {format_date} = require('../utils/helpers');
+const {format_date, format_plural, format_url} = require('../utils/helpers');
 
 test('format_date() returns a date string', () => {
     const date = new Date('2020-03-20 16:12:03');
@@ -6,11 +6,13 @@ test('format_date() returns a date string', () => {
     expect(format_date(date)).toBe('3/20/2020');
 });
 
-// test('format_plural() returns a plural string', () => {
-//     const points = new points('2020-03-20 16:12:03');
+test('format_plural() returns a pluralized word', () => {
+    const word1 = format_plural('tiger', 1);
+    const word2 = format_plural('lion', 2);
 
-//     expect(format_date(date)).toBe('3/20/2020');
-// });
+    expect(word1).toBe('tiger');
+    expect(word2).toBe('lions');    
+});
 
 test('format_url() returns a simplified url string', () => {
     const url1 = format_url('http://test.com/page/1');
