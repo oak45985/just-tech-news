642e26dad576d557e0bd6aaf7ece1c0154a0957e
oak45985 <oak45985@gmail.com>
2022-08-31 14:58:49 -0500

    updated for helpers and partials

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const PORT = process.env.PORT || 3001;
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const helpers = require('./utils/helpers');
+
 require('dotenv').config();
 const sess = {
     secret: process.env.DB_SECRET,
@@ -22,7 +24,7 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
