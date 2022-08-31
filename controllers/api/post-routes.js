b3a5658866c6304a5afc895d3c3eff6f96499a9e
oak45985 <oak45985@gmail.com>
2022-08-30 21:43:58 -0500

    single post complete

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -87,12 +87,16 @@ router.post('/', (req, res) => {
 
 //PUT /api/posts/upvote
 router.put('/upvote', (req, res) => {
-    Post.upvote(req.body, { Vote })
-        .then(updatedPostData => res.json(updatedPostData))
+    //make sure session exists first
+    if(req.session) {
+        //pass session id along with all destructed properties on req.body
+        Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+        .then(updatedVoteData => res.json(updatedVoteData))
         .catch(err => {
             console.log(err);
-            res.status(400).json(err);
-    });
+            res.status(500).json(err);
+        });
+    }
 });
 
 router.put('/:id', (req, res) => {
